6d14b609311c732857b88d6a6e4b168f6471df4d
Bui Minh Chien <minhchien0207@gmail.com>
2023-11-28 13:19:15 +0700

    update file ts

--- demo1.ts
+++ demo1.ts
@@ -1,39 +1,39 @@
 class Hocsinh {
-    public hoten:string;
-    public namsinh:number;
-    public mahocsinh:string;
-    private sothich:string;
-    
-    constructor(name:string, year:number, userno:string) {
-        this.hoten = name;
-        this.namsinh = year;
-        this.mahocsinh = userno;
-    }
+  public hoten: string;
+  public namsinh: number;
+  public mahocsinh: string;
+  private sothich: string;
 
-    setFavorite(fv:string) {
-        this.sothich = fv;
-    }
+  constructor(name: string, year: number, userno: string) {
+    this.hoten = name;
+    this.namsinh = year;
+    this.mahocsinh = userno;
+  }
 
-    getFavorite() {
-        return this.sothich;
-    }
+  setFavorite(fv: string) {
+    this.sothich = fv;
+  }
+
+  getFavorite() {
+    return this.sothich;
+  }
 }
 
 class bosung extends Hocsinh {
-    private diemso:number;
+  private diemso: number;
 
-    setDiem(n:number) {
-        this.diemso = n;
-    }
+  setDiem(n: number) {
+    this.diemso = n;
+  }
 
-    getDiem() {
-        return this.diemso;
-    }
+  getDiem() {
+    return this.diemso;
+  }
 }
 
-var teo:Hocsinh = new Hocsinh("Nguyễn Văn Tèo",2000,"HS0000001");
-teo.setFavorite("Xem film");
+const teo: Hocsinh = new Hocsinh('Nguyễn Văn Tèo', 2000, 'HS0000001');
+teo.setFavorite('Xem film');
 console.log(teo.getFavorite());
-var tun:bosung = new bosung("Nguyễn Văn Tũn",2000,"HS0000002");
+const tun: bosung = new bosung('Nguyễn Văn Tũn', 2000, 'HS0000002');
 tun.setDiem(10);
 console.log(tun.getDiem());
